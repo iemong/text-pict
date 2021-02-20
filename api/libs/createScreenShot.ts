94ef8b4160841bf5b7d4667f313d9520a7d4c2fa
iemong <kiyoshidainagon@gmail.com>
2021-02-20 22:57:56 +0900

    headeless chromeの環境をいい感じに出し分ける

--- api/libs/createScreenShot.ts
+++ api/libs/createScreenShot.ts
@@ -1,11 +1,27 @@
 import puppeteer from "puppeteer-core"
+import chrome from "chrome-aws-lambda"
+
+const isDev = !process.env.AWS_REGION
 
 export const createScreenShotFromHtml = async (
   html: string,
   width = 1200,
   height = 630
 ) => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch(
+    isDev
+      ? {
+          args: [],
+          executablePath:
+            "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+          headless: true,
+        }
+      : {
+          args: chrome.args,
+          executablePath: await chrome.executablePath,
+          headless: chrome.headless,
+        }
+  )
   const page = await browser.newPage()
   await page.setViewport({ width, height })
   await page.setContent(html)
