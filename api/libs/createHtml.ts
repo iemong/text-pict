4a9acaa047373cd4046cad3a9bd719e808df31a6
iemong <kiyoshidainagon@gmail.com>
2022-03-02 09:53:45 +0900

    fix for ticket

--- api/libs/createHtml.ts
+++ api/libs/createHtml.ts
@@ -1,10 +1,11 @@
-import { defaultHtml } from "api/templates/default"
-import sanitizeHtml from "sanitize-html"
+import { defaultHtml } from "../templates/default"
+import { ticketGeneratorHtml } from "../templates/ticketGenerator"
 
 type Option = {
   text: string
   width: number
   fontSize: number
+  type: string
 }
 
 export type templateOptions = {
@@ -15,17 +16,27 @@ export type templateOptions = {
 }
 
 export const createHtml = (option: Option) => {
-  const { text, width, fontSize } = option
+  const { text, width, fontSize, type } = option
 
   const calcVW = (size: number) => `${(size / width) * 100}vw`
   const actualFontSize = calcVW(fontSize)
   const borderSize = calcVW(40)
   const paddingSize = calcVW(50)
 
-  return defaultHtml({
-    text,
-    borderSize,
-    actualFontSize,
-    paddingSize,
-  })
+  switch (type) {
+    case "ticket":
+      return ticketGeneratorHtml({
+        text,
+        borderSize,
+        actualFontSize,
+        paddingSize,
+      })
+    default:
+      return defaultHtml({
+        text,
+        borderSize,
+        actualFontSize,
+        paddingSize,
+      })
+  }
 }
