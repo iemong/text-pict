593c0c1c9710d21879267d5fdeccb8ecbc366b47
iemong <kiyoshidainagon@gmail.com>
2022-03-02 09:20:46 +0900

    htmlを別のファイルに分ける

--- api/libs/createHtml.ts
+++ api/libs/createHtml.ts
@@ -1,3 +1,4 @@
+import { defaultHtml } from "api/templates/default"
 import sanitizeHtml from "sanitize-html"
 
 type Option = {
@@ -6,50 +7,25 @@ type Option = {
   fontSize: number
 }
 
+export type templateOptions = {
+  text: string
+  borderSize: string
+  actualFontSize: string
+  paddingSize: string
+}
+
 export const createHtml = (option: Option) => {
   const { text, width, fontSize } = option
 
-  const calcVW = (size:number) => `${size / width * 100}vw`
+  const calcVW = (size: number) => `${(size / width) * 100}vw`
   const actualFontSize = calcVW(fontSize)
   const borderSize = calcVW(40)
   const paddingSize = calcVW(50)
 
-  return `
-        <html lang="ja">
-            <head>
-                <meta charset="utf-8">
-                <title>Generated Image</title>
-                <style>
-                  @import url('https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap');
-                </style>
-                <style>
-                  body {
-                    margin: 0;
-                    padding: 0;
-                    background: #ffefa1;
-                    border: ${borderSize} solid #6ddccf;
-                    box-sizing: border-box;
-                    font-size: ${actualFontSize};
-                    font-family: "Yusei Magic", "Roboto", sans-serif;
-                    font-weight: bold;
-                    color: #555;
-                  }
-                  .flex {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    width: 100%;
-                    height: 100%;
-                    padding: ${paddingSize};
-                    box-sizing: border-box;
-                  }
-                </style>
-            </head>
-            <body>
-              <div class="flex">
-                ${sanitizeHtml(text)}
-              </div>
-            </body> 
-        </html>
-    `
+  return defaultHtml({
+    text,
+    borderSize,
+    actualFontSize,
+    paddingSize,
+  })
 }
